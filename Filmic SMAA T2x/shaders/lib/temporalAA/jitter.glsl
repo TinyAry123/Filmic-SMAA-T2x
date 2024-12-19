91cbb0b6f85eb4bb6747d6d5012d79e98b84afb6
TinyAry123 <aryamank495@gmail.com>
2024-12-19 03:11:11 -0700

    Temporary deletion for 2nd full-scale upload.

--- Filmic SMAA T2x/shaders/lib/temporalAA/jitter.glsl
+++
@@ -1,3 +0,0 @@
-void jitterTAA(inout vec4 clipSpacePosition) {
-    clipSpacePosition.xy += 2.0 * clipSpacePosition.w * jitterOffsetsTAA[frameMod2]; // Offsets should be in [-w, w] clip space range. 
-}
